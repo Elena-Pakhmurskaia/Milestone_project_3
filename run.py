90b792fae7be1836d151a6156f0c5fe4f1743eb9
Elena-Pakhmurskaia <elenapakhmurskaia@gmail.com>
2020-11-15 17:21:21 +0000

    added static folder with a template

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/hot_locations")
+def about():
+    return render_template("hot_locations.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+    
